316490dfb2353d484e62ee2433f7b3acf915217b
Steven Sweeny <69608159+Sweeny91@users.noreply.github.com>
2021-10-22 04:40:55 -0500

    fixing merge conflicts

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+// this function is used to open all external links in their own respective tabs
+// Read more: https://html.com/attributes/a-target/#ixzz7A0XctpNX
+function externalLinks() {
+    for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+        var b = c[a];
+        b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+    }
+};
+externalLinks();
